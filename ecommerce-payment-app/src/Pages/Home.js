1ab7fe409bd1fdd683ed0829c3820edad29e278a
b-khadija <khadija.bayssir@gmail.com>
2023-11-08 21:02:43 +0100

    fix : center cards

--- ecommerce-payment-app/src/Pages/Home.js
+++ ecommerce-payment-app/src/Pages/Home.js
@@ -15,19 +15,21 @@ function Home({ addToCart }) {
   }, []);
 
   return (
-    <div className="bg-white mx-auto">
-      <h2 className="text-2xl font-bold tracking-wide text-center mt-12">
-        Nos produits
-      </h2>
-      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        {products.map((product) => (
-          //Création d'un composant "Product" Pour chaque produit dans "products", avec les détails du produit
-          <Product
-            key={product.id}
-            product={product}
-            addToCart={() => addToCart(product)} //Ajouter le produit au panier
-          />
-        ))}
+    <div className="bg-white">
+      <div className="mx-auto w-full px-4 py-16 sm:px-6 sm:py-24 lg:w-full lg:px-8">
+        <h2 className="text-2xl font-bold tracking-wide text-center mt-12">
+          Nos produits
+        </h2>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            //Création d'un composant "Product" Pour chaque produit dans "products", avec les détails du produit
+            <Product
+              key={product.id}
+              product={product}
+              addToCart={() => addToCart(product)} //Ajouter le produit au panier
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
